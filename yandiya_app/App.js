aa18fad6af70ac3c1b48a0f1b7480db82e1df1b4
marqueese <115721274+marqueese@users.noreply.github.com>
2023-02-07 10:43:31 +0000

    stuff is here

--- yandiya_app/App.js
+++ yandiya_app/App.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState, TouchableOpacity} from "react";
+import React, { useEffect, useState, TouchableOpacity, useRef} from "react";
 import {View,Text,StyleSheet,Button,ScrollView,SafeAreaView,StatusBar,Image,TextInput,setInput,Pressable,ImageBackground, AppRegistry,Platform,input,Checkbox} from "react-native";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { useFonts } from "expo-font";
 const image = {uri: "https://i.imgur.com/A8WIsR2.png"};
-const icon = {uri: ''};
+const icon = {uri: 'https://i.imgur.com/5QVr3RA.png'};
 
 
 function homeScreen({ navigation }) {
@@ -14,10 +14,13 @@ function homeScreen({ navigation }) {
         <ImageBackground source={image} style={styles.image}/>
      <View style ={{height: 30, width: 260, backgroundColor: '#e42c22', borderRadius: 20, position: "absolute", left:10, top: 50}}>
     </View>
- <View style={{height: 100, width: 220, backgroundColor:  "#252525", border: "black", position: "absolute", left: 30, top: 320, borderRadius: 20, boxShadow: '0px 0px 18px #252525'}}>
+    <View>
+    <ImageBackground source={icon} style={styles.icon}/>
+    </View>
+ <View style={{height: 100, width: 220, backgroundColor:  "#252525", border: "black", position: "absolute", left: 30, top: 320, borderRadius: 20, boxShadow: '0px 0px 18px #252525',}}>
               <Button color = "#212121" onPress={() => navigation.navigate("Sign in")}title ="sign Up"/>
               <View style = {styles.space}/>
-              <Button style ={styles.logIn} color = "#212121" onPress={() => navigation.navigate("login")}title ="log in"/>
+              <Button style ={styles.logIn} color = "#212121" onPress={() => navigation.navigate("Login")}title ="log in"/>
              </View>
     </View>
   );
@@ -29,13 +32,10 @@ function mainScreen({ navigation }) {
     <View style ={{height: 30, width: 280, backgroundColor: '#e42c22', borderRadius: 20, position: "absolute", left:1, top: 60}}>
     <Text style = {{color: "white", fontSize: 18, position: "absolute", left: 90,}}>Home Screen</Text>
      <View style={{height: 270, width: 260, backgroundColor: "#ffffff", border: "black", position: "absolute", left: 10, top: 90, borderRadius: 20, boxShadow: '10px 10px 40px #454545',}}>
-      <Text style ={styles.box}>hello there</Text>
-      <Text style ={styles.box1}>hello there</Text>
+      <Text onClick ={() => navigation.navigate("commission")} style ={styles.box}>bo's diner: 2 celing pannels, managers office click here</Text>
+      <Text onClick ={() => navigation.navigate("commission2")} style ={styles.box1}>Marcus's excuse for stupidity'</Text>
        </View>
     </View>
-          <View style ={{position: "absolute", top: 330, right: 80}}>
-          <Button color = "#212121" onPress={() => navigation.navigate("commisions")}title ="commisions"/>
-        </View>
         <View style ={{position: "absolute", top: 370, right: 95}}>
           <Button color = "#212121" onPress={() => navigation.navigate("Settings")}title ="Settings"/>
         </View>
@@ -54,6 +54,7 @@ function loginScreen({ navigation }) {
       </Text>
       <TextInput
         style={{ padding: 8, backgroundColor: '#f5f5f5' }}
+          placeholder = "enter here"
         onChangeText={text => setName(text)}
       />
        <Text style={{ marginVertical: 16 }}>
@@ -61,6 +62,7 @@ function loginScreen({ navigation }) {
       </Text>
       <TextInput
         style={{ padding: 8, backgroundColor: '#f5f5f5' }}
+          placeholder = "enter here"
         onChangeText={text => setName(text)}
         secureTextEntry
       />
@@ -75,7 +77,7 @@ function loginScreen({ navigation }) {
   </View>
   );
 }
-function SignInScreen({ navigation }) {
+function signInScreen({ navigation }) {
   return (
         <View style ={styles.container}>
       <ImageBackground source={image} style={styles.image}/>
@@ -87,6 +89,7 @@ function SignInScreen({ navigation }) {
       </Text>
       <TextInput
         style={{ padding: 8, backgroundColor: '#f5f5f5' }}
+          placeholder = "enter here"
         onChangeText={text => setName(text)}
       />
        <Text style={{ marginVertical: 16 }}>
@@ -94,6 +97,7 @@ function SignInScreen({ navigation }) {
       </Text>
       <TextInput
         style={{ padding: 8, backgroundColor: '#f5f5f5' }}
+        placeholder = "enter here"
         onChangeText={text => setName(text)}
         secureTextEntry
       />
@@ -103,6 +107,7 @@ function SignInScreen({ navigation }) {
       <TextInput
         style={{ padding: 8, backgroundColor: '#f5f5f5' }}
         onChangeText={text => setName(text)}
+          placeholder = "enter here"
         secureTextEntry
       />
       
@@ -119,25 +124,160 @@ function SignInScreen({ navigation }) {
 }
 function settingsScreen({ navigation }) {
   return (
-
     <View style ={styles.container}>
-           <Text style={styles.text}>Settings</Text>
-        <View style ={{position: "absolute", top: 20, right: 20}}>
-          <Button color = "#212121" onPress={() => navigation.navigate("main")}title ="Home"/>
+    <ImageBackground source={image} style={styles.image}/>
+    <View style ={{height: 30, width: 280, backgroundColor: '#e42c22', borderRadius: 20, position: "absolute", left:1, top: 60}}>
+    <Text style = {{color: "white", fontSize: 18, position: "absolute", left: 115,}}>Settings</Text>
+     <View style ={{position: "absolute", top: 110, right: 110}}>
+        <Button color = "#212121" onPress={() => navigation.navigate("main")}title ="Home"/>
         </View>
-        <Text style={{position: "absolute", top: 120, right:40, color: "white"}}> are you sure you want to logout</Text>
-          <View style ={{position: "absolute", top: 150, right:100}}>
-          <Button color = "#212121" onPress={() => navigation.navigate("Home")}title ="Logout"/>
-        </View>
-      </View>
-);
-function commisionScreen({ navigation }) {
-  return (
-
-    <View style ={styles.container}>
-           <Text style={styles.text}>Commisions</Text>
-        <View style={styles.settings}> 
-          <Button color = "#212121" onPress={() => navigation.navigate("main")}title ="Home"/>
-        </View>
+        <View style ={{position: "absolute", top: 150, right:100}}>
+        <Button color = "#212121" onPress={() => navigation.navigate("logout")}title ="Logout"/>
+         <View style ={{position: "absolute", top: 40, right:10}}>
+          <div>
+      <Check/>
+    </div>
     </View>
-);
+      </View>
+    </View>
+    </View>
+  )
+};
+function commissionScreen({ navigation }) {
+  return (
+    <View style ={styles.container}>
+      <ImageBackground source={image} style={styles.image}/>
+    <View style ={{height: 30, width: 280, backgroundColor: '#e42c22', borderRadius: 20, position: "absolute", left:1, top: 60}}>
+    <Text style = {{color: "white", fontSize: 18, position: "absolute", left: 90,}}>Bo's Commission</Text>
+     <View style={{height: 270, width: 260, backgroundColor: "#ffffff", border: "black", position: "absolute", left: 10, top: 90, borderRadius: 20, boxShadow: '10px 10px 40px #454545',}}>
+       </View>
+    </View>
+        <View style = {{position: "absolute", left: 220, top: 155,}}> 
+          <Button color = "#212121" onPress={() => navigation.navigate("main")}title ="<--"/>
+         <View style = {{position: "absolute", left: -200, top: 40,}}> 
+        </View>
+        </View>
+  </View>
+)
+  };
+  function commissionScreen2({ navigation }) {
+  return (
+ <View style = {{backgroundColor: "whitesmoke"}}>
+   <div>
+  <SignatureBox/> 
+   </div>
+   <View>
+    <Button color = "#212121" onPress={() => navigation.navigate("main")}title ="Get back to work"/>
+    </View>
+  </View>
+)
+  };
+  function logoutScreen({ navigation }) {
+    return (
+    <View style ={styles.container}>
+    <View style={[styles.bannerBox]}>
+    <Text style={[styles.boxFont,{fontSize:24,textTransform:"uppercase",color:"white"},]}>Yandiya</Text>
+    </View>
+    <View style={[styles.space]}/>
+    <View style={[styles.middleBox,styles.shadow]}>
+    <Text style={[styles.boxFont,{fontSize:19,color:"#212121"}]}>
+    <b>Are you sure you want to log out?</b>
+    </Text>
+    <View style={styles.fixToText}>
+    <View style={styles.shadow}>
+    <Button color="#32aa46"  onPress={() => navigation.navigate("Home")}title ="Yes"/>
+    </View>
+    <View style={styles.shadow}>
+    <Button color="#e42c22" onPress={() => navigation.navigate("main")}title ="No"/>
+    </View>
+    </View>
+    </View>
+    <View style={[styles.space]}/>
+    </View>
+  
+  )
+    };
+  
+const Drawer = createDrawerNavigator();
+
+const SignatureBox = () => {
+  const [isDrawing, setIsDrawing] = useState(false);
+  const canvasRef = useRef(null);
+
+  const startDrawing = (event) => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+
+    context.strokeStyle = 'black';
+    setIsDrawing(true);
+    context.moveTo(event.clientX, event.clientY);
+  };
+
+  const continueDrawing = (event) => {
+    if (!isDrawing) {
+      return;
+    }
+
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+
+    context.lineTo(event.clientX, event.clientY);
+    context.stroke();
+  };
+
+  const stopDrawing = () => {
+    setIsDrawing(false);
+  };
+
+  return (
+    <div>
+      <canvas
+        ref={canvasRef}
+        width={250}
+        height={441}
+        onMouseDown={startDrawing}
+        onMouseMove={continueDrawing}
+        onMouseUp={stopDrawing}
+        onMouseLeave={stopDrawing}
+      />
+        <button onClick={() => canvasRef.current.getContext('2d').clearRect(0, 0, canvasRef.current.width, canvasRef.current.height)}>
+      Clear
+    </button>
+    </div>
+  );
+};
+
+const Check = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  return (
+    <div>
+      <input
+       style = {{height: 20, width: 20}}
+        type="checkbox"
+        checked={isChecked}
+        onChange={() => setIsChecked(!isChecked)}
+      />
+    </div>
+  );
+};
+
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={homeScreen} />
+        <Drawer.Screen name="main" component={mainScreen} />
+        <Drawer.Screen name="Sign in" component={signInScreen} />
+        <Drawer.Screen name="Login" component={loginScreen} />
+        <Drawer.Screen name="commission2" component={commissionScreen2} />
+        <Drawer.Screen name="commission" component={commissionScreen} />
+        <Drawer.Screen name="Settings" component={settingsScreen} />
+        <Drawer.Screen name="logout" component={logoutScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
