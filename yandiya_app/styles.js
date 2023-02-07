d4eda739898776572efc22fe58aec00fc1de4d35
marqueese <115721274+marqueese@users.noreply.github.com>
2023-02-07 10:44:41 +0000

    heres more stuff you ass

--- yandiya_app/styles.js
+++ yandiya_app/styles.js
@@ -1,17 +1,17 @@
-import { StyleSheet } from 'react-native';
-
-const styles = StyleSheet.create({ //Export const
+const styles = StyleSheet.create({
     container: {
+      backgroundColor: "#f8f7f7"
       },
       image: {
-       width: 282,
-       height: 500,
+       width: 190,
+       height: 200,
        flex: 1,
+       position: 'absolute', top: -45, left: 92,
       },
-      image2: {
+      icon: {
        width: 200,
        height: 200,
-       position: 'absolute', top: -30, left: -60,
+       position: 'absolute', top: 60, right: 35,
       },
        image3: {
        width: 250,
@@ -53,7 +53,7 @@ const styles = StyleSheet.create({ //Export const
         borderRadius: 20,
     },
     box5:{
-        backgroundColor: "#5e1313",
+        backgroundColor: "#E42c22",
         width: 270,
         height: 80,
         position: 'absolute', top: 30, right: 5,
@@ -65,50 +65,124 @@ const styles = StyleSheet.create({ //Export const
         position: 'absolute', top: 20, right: 40,
         fontWeight: "bold",
         color: "white",
-     },
-        home:{
-        backgroundColor: "whitesmoke",
-        width: 60,
-        height: 60,
-        position: 'absolute', top: 410, right: 5,
-        borderRadius: 10,
-      },
-       files:{
-        backgroundColor: "whitesmoke",
-        width: 60,
-        height: 60,
-        position: 'absolute', top: 410, right: 75,
-        borderRadius: 10,
-      },
-       client:{
-        backgroundColor: "whitesmoke",
-        width: 60,
-        height:60,
-        position: 'absolute', top: 410, right: 145,
-        borderRadius: 10,
-      },
-       settings:{
-        backgroundColor: "whitesmoke",
-        width: 60,
-        height: 60,
-        position: 'absolute', top: 410, right: 215,
-        borderRadius: 10,
       },
       inc:{
-        color: 'white',
+        color: 'black',
          position: 'absolute', top: 150, right: 50,
          fontSize: 18,
          fontWeight: "bold"
       },
-      continue:{
-      flex: 1, alignItems: 'center', justifyContent: 'center',
-               position: 'absolute', top: 400, right: 90
-      },
-      Home:{
+      commisions:{
          flex: 1, alignItems: 'center', justifyContent: 'center',
-               position: 'absolute', top: 460, right: 220
+               position: 'absolute', top: 460, right: 170
+      },
+      email:{
+        flex: 1,alignContent: 'center',justifyContent: 'center', padding: 16, position: 'absolute', top: 120, right: 40
+      },
+        text2: {
+        color: 'white',
+        fontSize: 18,
+        lineHeight: 84,
+        textAllign: 'right',
+        position: 'absolute', top: -39, left:90,
+        fontWeight: "bold",
+    },
+    epassword:{
+        flex: 1,alignContent: 'center',justifyContent: 'center', padding: 16, position: 'absolute', top: 260, right: 40
+      },
+        text3: {
+        color: 'white',
+        fontSize: 18,
+        lineHeight: 84,
+        textAllign: 'right',
+        position: 'absolute', top: 30, left:90,
+        fontWeight: "bold",
+    },
+    signUp:{
+      flex: 1, alignItems: 'center', justifyContent: 'center',
+               position: 'absolute', top: 300, right: 94, color: "#212121",
+      },
+      logIn:{
+      flex: 1, alignItems: 'center', justifyContent: 'center',
+               position: 'absolute', top: 360, right: 90,
+      },
+      Back:{
+      flex: 1, alignItems: 'center', justifyContent: 'center',
+               position: 'absolute', top:-100, right: -20,
+      },
+      Bank:{
+      flex: 1, alignItems: 'center', justifyContent: 'center',
+               position: 'absolute', top:-150, right: -10,
+      },
+      settings:{
+            flex: 1, alignItems: 'center', justifyContent: 'center',
+               position: 'absolute', top: 460, right: 10,
+      },
+      space:{
+        flex: 1,
+      },
+      box:{
+        height: 50,
+        width: 220,
+        backgroundColor: "#000000",
+        position: "absolute",
+        left: 20,
+        top: 20,
+       color:"white",
+       borderRadius: 20,
+       textAlign: "center",
+       flex: 1,
+      },
+      box1:{
+        height: 30,
+        width: 220,
+        backgroundColor: "#000000",
+        position: "absolute",
+        left: 20,
+        top: 90,
+       color:"white",
+       borderRadius: 20,
+       textAlign: "center",
+       flex: 1,
+      },
+      bannerBox: {
+        flex: 5, 
+        justifyContent: "center",
+        backgroundColor: "#e42c22",
+      },
+      space: { 
+        flex:1,
+      },
+      middleBox: { 
+        backgroundColor: "#f8f7f7", 
+        justifyContent: "center", 
+        flex: 10, 
+        width: 250, 
+        marginLeft: 15, 
+        borderRadius: 40,
+      }, 
+      fixToText: {
+        marginTop: 40,
+        flexDirection: "row",
+        justifyContent: "space-between", 
+        textAlign: "center", 
+        marginBottom: 50, 
+        marginLeft: 30, 
+        marginRight: 30,
+      }, 
+      boxFont: { 
+        justifyContent: "center",
+         textAlign: "center", 
+      }, 
+      shadow: { 
+       shadowColor: "#000000",
+       top: 100,
+      }, 
+      shadowOffset:{ 
+       width: 0, 
+       height: 9,
+       shadowOpacity: 0.25, 
+       shadowRadius: 40,
+       elevation: 18,
       }
-
-})
-
-export {styles}
+    });
